3d1927285778546cadaa6e5035a0e247f7393a33
Rohin Galhotra <jiminycracken@gmail.com>
2018-03-27 09:39:49 -0700

    added heuristic function

--- src/hw03_rgalhotra.cpp
+++ src/hw03_rgalhotra.cpp
@@ -3,6 +3,24 @@
 #include <vector>
 #include <algorithm>
 
+int calcH(std::vector<char> state) {
+	unsigned int i = 0;
+	unsigned int j = 0;
+	int num = 0;
+
+	for (i = 0; i < state.size(); i++) {
+		if (state.at(i) == 'W') {
+			for (j = i; j < state.size(); j++) {
+				if (state.at(j) == 'B') {
+					num++;
+					break;
+				}
+			}
+		}
+	}
+	return num;
+}
+
 int main(void) {
 
 	std::string in;
